f3d797009a2d8ec87a4dbc688fcc4c98f84c1f18
Akshat Khosya <akshatdps12@gmail.com>
2023-12-27 14:49:19 +0530

    added ValidationPipe global and user create api

--- server/src/config/configuration.ts
+++ server/src/config/configuration.ts
@@ -1,4 +1,5 @@
 export default () => ({
   PORT: parseInt(process.env.PORT, 10) || 4000,
   MONGODB_URL: process.env.MONGODB_URL || '',
+  saltRounds: parseInt(process.env.SALT) || 10,
 });
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
-
+import { User, UserDocument } from './schema/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    private readonly configService: ConfigService,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    const saltOrRounds = this.configService.get<number>('saltRounds');
+    createUserDto.password = await bcrypt.hash(
+      createUserDto.password,
+      saltOrRounds,
+    );
+    await this.userModel.create({
+      ...createUserDto,
+      createdAt: new Date(),
+    });
+  }
+}
